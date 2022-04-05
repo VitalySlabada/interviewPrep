67b02a2b4dab08e24ff14c87308eea6a5b1c0c53
VitalySlabada <vitaly.slabada@gmail.com>
2022-04-04 20:57:08 -0500

    Initial Commit prob1 and 2

---
+++ src/main/java/DSA/P2_ArrayOfProducts/aopMain.java
@@ -0,0 +1,20 @@
+package DSA.P2_ArrayOfProducts;
+
+import java.util.ArrayList;
+import static DSA.P2_ArrayOfProducts.ArrOfProducts.*;
+
+public class aopMain {
+    public static void main(String[] args) {
+        
+        int[] givenArr = {5, 1, 4, 2};
+
+        ArrayList<Integer> result = findResultArr(
+                givenArr,
+                createCumulProdMap(givenArr),
+                createCumulProdMapReveresed(givenArr)
+        );
+
+        System.out.println("result = " + result);
+        
+    }
+}
